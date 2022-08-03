b6c0fc3b87d410d45fb56067590c6da91cc65518
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-08-03 16:44:16 +0900

    initial commit

--- app.js
+++ app.js
@@ -17,10 +17,35 @@ const http = require("http");
 const app = http.createServer((req, res)=>{
     res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"})
     if (req.url === '/'){
-        res.end("여기는 루트 화면입니다. ");
+        res.end(`
+        <!DOCTYPE html>
+        <html lang="ko">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <h1>여기는 루트 화면입니다. </h1>
+            </body>
+        </html>`);
     }
     else if(req.url === '/login'){
-        res.end("여기는 로그인 화면입니다. ");
+        res.end(`
+        <!DOCTYPE html>
+        <html lang="ko">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+            <body>
+                <input type="text" placeholder="아이디"><br>
+                <input type="password" placeholder="비밀번호"><br>
+                <button>로그인</button>
+            </body>
+        </html>`
+        );
     }
     console.log(req.url);
 });
