9edcc9769a2aed9eeafca739b3ccee70dbac94cc
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-08-10 14:10:51 +0900

    initial commit

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+"use strict";
+
 const express = require("express");
 const app = express();
+const PORT = 3000;
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res)=>{
-    res.render("home/index");
-});
-app.get("/login", (req, res)=>{
-    res.render("home/login");
-});
+const home = require("./routes/home");
+app.use("/", home); //use = 미들웨어를 등록해준다. 
 
-app.listen(3000, function(){
-    console.log("서버 가동");
-})
+module.exports = app;
