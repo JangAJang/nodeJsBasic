02711dd18bac1b1c45d0f6fe4a90e6f05f20882a
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-08-14 14:51:18 +0900

    initial commit
    js 로그인 기본 구현

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ const home = require("./src/routes/home");
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-const PORT = 3000;
-app.listen(PORT, function(){
-    console.log("서버 가동");
-});
+
 app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); //use = 미들웨어를 등록해준다. 
 
--- bin/www.js
+++ bin/www.js
@@ -1,2 +1,7 @@
-"user strict"
+"use strict";
 
+const app = require("../app");
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log("서버 가동");
+});
